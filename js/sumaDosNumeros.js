9efe0279e107a9f3a4cccec51354834173723965
LourdesFrrl <ferraiolimarialourdesfacultad@gmail.com>
2025-04-13 18:26:43 -0300

    agrege el archivo js que suma dos numeros

---
+++ js/sumaDosNumeros.js
@@ -0,0 +1,3 @@
+let numero1 = parseFloat(prompt("Ingrese el primer numero"));
+let numero2 = parseFloat(prompt("Ingrese el segundo numero"));
+document.writeln("La suma de ambos numero es ", numero1 + numero2);
