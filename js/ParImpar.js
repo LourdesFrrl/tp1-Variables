5a679954201c075580ed50aaa57243cdf1216bb1
LourdesFrrl <ferraiolimarialourdesfacultad@gmail.com>
2025-04-13 18:22:40 -0300

    agrege el archivo js que determina si un nro es par o impar

---
+++ js/ParImpar.js
@@ -0,0 +1,8 @@
+let numero = parseInt(prompt("Ingrese un numero"))
+let modulo = numero % 2
+if(modulo==0){
+    document.writeln("El numero ", numero ," es par")
+}
+else{
+    document.writeln("El numero ", numero ," es impar")
+}
